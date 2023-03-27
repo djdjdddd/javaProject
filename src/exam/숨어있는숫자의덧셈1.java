b394c25f4c5943327843219d67629b760b1bba0e
djdjdddd <djdj2297@gmail.com>
2023-03-27 17:38:16 +0900

    3.27

--- src/exam/숨어있는숫자의덧셈1.java
+++ src/exam/숨어있는숫자의덧셈1.java
@@ -3,20 +3,18 @@ package exam;
 public class 숨어있는숫자의덧셈1 {
 	
 	class Solution {
-	    public int solution(String my_string) {
-	        int answer = 0;
+	    public String solution(String my_string) {
+	        String answer = "";
 	        char ch;
-		    int sum = 0;
-	        String str = "";
 	        
-		    for(int i=0; i<my_string.length(); i++){
-	            ch = my_string.charAt(i);
-	            if(Character.isDigit(ch)){
-	            	str = Character.toString(ch);
-	                sum += Integer.parseInt(str);
-	            }
+	        for(int i=0; i<my_string.length(); i++) {
+	        	ch = my_string.charAt(i);
+	        	String str = Character.toString(ch);
+//	        	if(str.matches([A-z])) {
+//	        		
+//	        	}
 	        }
-	        answer = sum;
+	        
 	        return answer;
 	    }
 	}
